6cbad62f1cf2b9f8163790a6b3e7753acc67d90b
suleyman1406 <dadasovsuleyman126@gmail.com>
2024-01-07 02:12:58 +0400

    feat: register action configured with db

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,11 @@
 "use server";
 
+import bcrypt from "bcrypt";
 import z from "zod";
 
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
+import { db } from "@/lib/db";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -10,6 +13,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
+
+  const { email, password, name } = validatedFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  const user = await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
 
   return { success: "Email sent!" };
 };
